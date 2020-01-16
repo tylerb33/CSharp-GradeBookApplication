168d032ec92e0b20613a1b5a628c48b50eebd7e4
Tyler Barnett <gci6251@hca.corpad.net>
2020-01-16 14:50:07 -0600

    latest

--- GradeBook/GradeBooks/RankedGradebook.cs
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -2,9 +2,9 @@
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradebook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradebook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
