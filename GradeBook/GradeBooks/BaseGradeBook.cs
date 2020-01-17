075fd0ced05836a87cbcaee1cef837faaa93d346
Tyler Barnett <gci6251@hca.corpad.net>
2020-01-17 12:47:27 -0600

    added

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -134,7 +134,7 @@ namespace GradeBook.GradeBooks
             if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
                 gpa++;
 
-            return 0;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
